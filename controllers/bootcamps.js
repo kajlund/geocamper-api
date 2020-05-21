6cdd5c353bc4278570f6540cac6ba50d7a210789
Kaj Lund <kaj.lund@gmail.com>
2020-05-21 19:35:59 +0300

    Add field select and sort

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,12 +9,36 @@ const wrap = require('../middleware/wrap');
 // @route  GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = wrap(async (req, res, next) => {
-  let queryStr = JSON.stringify(req.query);
+  let query;
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude from filtering
+  const removeFields = ['select', 'sort'];
+
+  // Delete removeFields from query
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery);
+  // Create operators ($gt, $gte...)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
   // log.info(queryStr);
-  const query = JSON.parse(queryStr);
+  query = Bootcamp.find(JSON.parse(queryStr));
 
-  const bootcamps = await Bootcamp.find(query);
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  const bootcamps = await query;
   res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
 });
 
